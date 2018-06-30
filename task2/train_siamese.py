b8bd2a78582c6c3c8e284b85ea536974d0994de1
tomHuang <tom.huang@positivegrid.com>
2018-06-30 23:08:05 +0800

    feat: tweak learning schedule
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- task2/train_siamese.py
+++ task2/train_siamese.py
@@ -8,6 +8,7 @@ from keras.regularizers import l2
 from keras.callbacks import EarlyStopping
 from keras.metrics import binary_accuracy
 from keras.backend.tensorflow_backend import set_session
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn.cross_validation import train_test_split
 import sys
 import os
@@ -15,7 +16,131 @@ import tensorflow as tf
 import io_data
 import numpy as np
 
+DATA_PATH = 'data/'
+NUM_BASE_CLASS = 80
+NUM_NOVEL_CLASS = 20
+NOVEL_SAMPLE = 5
+NUM_BASE_EXAMPLES = 500
+IMG_SIZE = 32
+BATCH_SIZE = 128
+MAX_EPOCH = 1000000
+NOVEL_RATIO = 4
+DISPLAY_RATIO = 301
+PATIENCE = 10
 
+def data_augm(imgs):
+	train_datagen = ImageDataGenerator(
+        rotation_range = 40,
+        width_shift_range = 0.2,
+        height_shift_range = 0.2,
+        shear_range = 0.2,
+        zoom_range = 0.2,
+        horizontal_flip = True,
+        fill_mode = 'nearest')
+	train_datagen.fit(imgs)
+	return train_datagen
+
+def load_pair_batch(data, train_datagen, mode1 = 'train', mode2 = 'base'):
+    novel_imgs, novel_label, base_imgs, base_label = data
+    novel_imgs = novel_imgs.reshape((NUM_NOVEL_CLASS, NOVEL_SAMPLE, IMG_SIZE, IMG_SIZE, 3))
+    base_imgs = base_imgs.reshape((NUM_BASE_CLASS, NUM_BASE_EXAMPLES, IMG_SIZE, IMG_SIZE, 3))
+
+    if mode1 == 'train' and mode2 == 'base':
+        # random choose category
+        categories = np.random.choice(NUM_BASE_CLASS, size = (BATCH_SIZE,), replace = True)
+        pairs = [np.zeros((BATCH_SIZE, IMG_SIZE, IMG_SIZE, 3)) for i in range(2)]
+        targets = np.zeros((BATCH_SIZE,))
+        same_idx = np.random.choice(BATCH_SIZE, size = (BATCH_SIZE//2,), replace = False)
+        targets[same_idx] = 1
+        for i in range(BATCH_SIZE):
+            category = categories[i]
+            idx_1 = np.random.randint(0, 400)
+            pairs[0][i,:,:,:] = base_imgs[category, idx_1].reshape(IMG_SIZE, IMG_SIZE, 3)
+            idx_2 = np.random.randint(0, 400)
+            if i in same_idx:
+                # same category
+                category2 = category
+            else:
+                # different category
+                category2 = (category + np.random.randint(1, NUM_BASE_CLASS)) % NUM_BASE_CLASS
+                #print('train: ', category, category2)
+            pairs[1][i,:,:,:] = base_imgs[category2, idx_2].reshape(IMG_SIZE, IMG_SIZE, 3)
+
+    if mode1 == 'train' and mode2 == 'novel':
+        # random choose category
+        categories = np.random.choice(NUM_NOVEL_CLASS, size = (BATCH_SIZE,), replace = True)
+        pairs = [np.zeros((BATCH_SIZE, IMG_SIZE, IMG_SIZE, 3)) for i in range(2)]
+        targets = np.zeros((BATCH_SIZE,))
+        same_idx = np.random.choice(BATCH_SIZE, size = (BATCH_SIZE//2,), replace = False)
+        targets[same_idx] = 1
+        for i in range(BATCH_SIZE):
+            category = categories[i]
+            idx_1 = np.random.randint(0, NOVEL_SAMPLE)
+            tmp = novel_imgs[category, idx_1].reshape(IMG_SIZE, IMG_SIZE, 3)
+            if np.random.randint(2, size=1) == 0:
+                pairs[0][i,:,:,:] = train_datagen.random_transform(tmp)
+            else:
+                pairs[0][i,:,:,:] = tmp
+
+            idx_2 = np.random.randint(0, NOVEL_SAMPLE)
+            if i in same_idx:
+                # same category
+                category2 = category
+            else:
+                # different category
+                category2 = (category + np.random.randint(1, NUM_NOVEL_CLASS)) % NUM_NOVEL_CLASS
+                #print('train: ', category, category2)
+            tmp = novel_imgs[category2, idx_2].reshape(IMG_SIZE, IMG_SIZE, 3)
+            if np.random.randint(2, size=1) == 0:
+                pairs[1][i,:,:,:] = train_datagen.random_transform(tmp)
+            else:
+                pairs[1][i,:,:,:] = tmp
+
+    if mode1 == 'valid' and mode2 == 'base':
+        # random choose category
+        categories = np.random.choice(NUM_BASE_CLASS, size = (BATCH_SIZE,), replace = True)
+        pairs = [np.zeros((BATCH_SIZE, IMG_SIZE, IMG_SIZE, 3)) for i in range(2)]
+        targets = np.zeros((BATCH_SIZE,))
+        same_idx = np.random.choice(BATCH_SIZE, size = (BATCH_SIZE//2,), replace = False)
+        targets[same_idx] = 1
+        for i in range(BATCH_SIZE):
+            category = categories[i]
+            idx_1 = np.random.randint(400, 500)
+            pairs[0][i,:,:,:] = base_imgs[category, idx_1].reshape(IMG_SIZE, IMG_SIZE, 3)
+            idx_2 = np.random.randint(400, 500)
+            if i in same_idx:
+                # same category
+                category2 = category
+            else:
+                # different category
+                category2 = (category + np.random.randint(1, NUM_BASE_CLASS)) % NUM_BASE_CLASS
+                #print('valid: ', category, category2)
+            pairs[1][i,:,:,:] = base_imgs[category2, idx_2].reshape(IMG_SIZE, IMG_SIZE, 3)
+
+    if mode1 == 'valid' and mode2 == 'novel':
+        # random choose category
+        categories = np.random.choice(NUM_NOVEL_CLASS, size = (BATCH_SIZE,), replace = True)
+        pairs = [np.zeros((BATCH_SIZE, IMG_SIZE, IMG_SIZE, 3)) for i in range(2)]
+        targets = np.zeros((BATCH_SIZE,))
+        same_idx = np.random.choice(BATCH_SIZE, size = (BATCH_SIZE//2,), replace = False)
+        targets[same_idx] = 1
+        for i in range(BATCH_SIZE):
+            category = categories[i]
+            idx_1 = np.random.randint(0, NOVEL_SAMPLE)
+            pairs[0][i,:,:,:] = novel_imgs[category, idx_1].reshape(IMG_SIZE, IMG_SIZE, 3)
+
+            idx_2 = np.random.randint(0, NOVEL_SAMPLE)
+            if i in same_idx:
+                # same category
+                category2 = category
+            else:
+                # different category
+                category2 = (category + np.random.randint(1, NUM_NOVEL_CLASS)) % NUM_NOVEL_CLASS
+                #print('train: ', category, category2)
+
+            pairs[1][i,:,:,:] = novel_imgs[category2, idx_2].reshape(IMG_SIZE, IMG_SIZE, 3)
+
+    return pairs, targets
 
 def compute_accuracy(preds, labels):
     '''Compute classification accuracy with a fixed threshold on distances.
@@ -60,14 +185,12 @@ if __name__ == '__main__':
 
     MODEL_PATH = str(sys.argv[2])
 
-    img_pairs, labels = io_data.read_train_pairwise('data/', total_num=50000)
+    novel_imgs, novel_label, base_imgs, base_label = io_data.read_train(DATA_PATH, sample = NOVEL_SAMPLE)
+    data = novel_imgs, novel_label, base_imgs, base_label
 
-    print('training img pairs shape: ', img_pairs.shape)
-    print('training labels shape: ', labels.shape)
+    train_datagen = data_augm(novel_imgs)
 
-    img_pairs_train, img_pairs_test, labels_train, labels_test = train_test_split(img_pairs, labels, test_size=.1)
-
-    input_dim = img_pairs_train.shape[2:]
+    input_dim = (IMG_SIZE, IMG_SIZE, 3)
     input_a = Input(shape=input_dim)
     input_b = Input(shape=input_dim)
     base_network = create_base_network(input_dim)
@@ -86,28 +209,45 @@ if __name__ == '__main__':
     print('\n\njoint network: \n')
     model.summary()
 
-    # train
-    epochs = 1000
     rms = RMSprop()
     model.compile(loss='binary_crossentropy', optimizer=rms, metrics=[binary_accuracy])
-    xtr1 = img_pairs_train[:, 0]
-    xtr2 = img_pairs_train[:, 1]
-    model.fit(
-        [xtr1, xtr2],
-        labels_train,
-        validation_split=.1,
-        batch_size=128,
-        epochs=epochs,
-        callbacks=[EarlyStopping(monitor='val_binary_accuracy', patience=10)])
 
-    model.save(MODEL_PATH, include_optimizer=False)
-    print('model saved')
+    curr_max_acc = 0.0
 
-    # compute final accuracy on training and test sets
-    pred = model.predict([img_pairs_train[:, 0], img_pairs_train[:, 1]]).reshape((-1,))
-    tr_acc = compute_accuracy(pred, labels_train)
-    pred = model.predict([img_pairs_test[:, 0], img_pairs_test[:, 1]]).reshape((-1,))
-    te_acc = compute_accuracy(pred, labels_test)
+    for i in range(MAX_EPOCH):
+        if i % NOVEL_RATIO == 0:
+            imgs, target = load_pair_batch(data, train_datagen, 'train', 'novel')
+        else:
+            imgs, target = load_pair_batch(data, train_datagen, 'train', 'base')
 
-    print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
-    print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
+        loss, acc = model.train_on_batch(imgs, target)
+
+        if i % DISPLAY_RATIO == 0:
+            imgs_val_base, target_val_base = load_pair_batch(data, train_datagen, 'valid', 'base')
+            imgs_ori_novel, target_ori_novel = load_pair_batch(data, train_datagen, 'valid', 'novel')
+            loss_val_base, acc_val_base = model.test_on_batch(imgs_val_base, target_val_base)
+            loss_ori_novel, acc_ori_novel = model.test_on_batch(imgs_ori_novel, target_ori_novel)
+            print("iteration {}, training loss: {:.3f}, training acc: {:.3f},\nbase val loss: {:.3f}, base val acc: {:.3f},\nnovel loss: {:.3f}, novel acc: {:.3f}".format(i, loss, acc, loss_val_base, acc_val_base, loss_ori_novel, acc_ori_novel))
+
+
+            # apply earlystopping
+            if acc_val_base > curr_max_acc:
+                curr_max_acc = acc_val_base
+                patience_cnt = 0
+                model.save(MODEL_PATH)
+            else:
+                patience_cnt += 1
+
+            if patience_cnt > PATIENCE and i > 20000:
+                print('Early stopping...')
+                break
+
+
+    # # compute final accuracy on training and test sets
+    # pred = model.predict([img_pairs_train[:, 0], img_pairs_train[:, 1]]).reshape((-1,))
+    # tr_acc = compute_accuracy(pred, labels_train)
+    # pred = model.predict([img_pairs_test[:, 0], img_pairs_test[:, 1]]).reshape((-1,))
+    # te_acc = compute_accuracy(pred, labels_test)
+
+    # print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
+    # print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
