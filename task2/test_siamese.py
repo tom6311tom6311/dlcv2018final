f4b2544dc4a470e326d3c453692f478bb82ebb97
tomHuang <tom.huang@positivegrid.com>
2018-06-30 16:27:02 +0800

    feat: modify test script to allow single similarity metric
    Signed-off-by: tomHuang <tom.huang@positivegrid.com>

--- task2/test_siamese.py
+++ task2/test_siamese.py
@@ -3,41 +3,55 @@ import os
 import sys
 import numpy as np
 # import pandas as pd
-from keras import backend as K
+from keras.backend.tensorflow_backend import set_session
 from keras.models import load_model
 import tensorflow as tf
 import io_data
 
 # path
-data_path = sys.argv[1]
-model_path = sys.argv[2]
-output_path = sys.argv[3]
+data_path = 'data/'
 
 # parameters
 sample = 5
 novel_class = 20
 img_size = 32
 
-def find_closest_class(dist, label, count=3):
-  idx_near_to_far = np.argsort(np.sum(np.square(dist), axis=1))
-  label_near_to_far = label[idx_near_to_far,].astype('int')
-  label_count = np.zeros((novel_class,))
-  for l in label_near_to_far:
-    if l <= novel_class:
-      label_count[l] += 1
-      if label_count[l] == count:
-        return l
+def progress(count, total, suffix=''):
+	bar_len = 60
+	filled_len = int(round(bar_len * count / float(total)))
+	percents = round(100.0 * count / float(total), 1)
+	bar = '#' * filled_len + '-' * (bar_len - filled_len)
+	sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
+	sys.stdout.flush()
+
+def find_closest_class(sim, label):
+	idx_sim_rank = np.argsort(sim)
+	label_sim_rank = label[idx_sim_rank,]
+
+	label_count = { 'null': 0 }
+	curr_closest = 'null'
+	for idx, l in enumerate(label_sim_rank):
+		if l not in label_count:
+			label_count[l] = idx
+		else:
+			label_count[l] += idx
+		if label_count[l] >= label_count[curr_closest]:
+			curr_closest = l
+	return curr_closest
+
 
 def compute_acc(pred, true):
-
 	return np.mean((pred == true).astype(int))
 
 
 if __name__ == '__main__':
+	os.environ["CUDA_VISIBLE_DEVICES"] = str(sys.argv[1])
 	config = tf.ConfigProto()
-	config.gpu_options.allow_growth = True
-	session = tf.Session(config=config)
-	K.set_session(session)
+	config.gpu_options.per_process_gpu_memory_fraction = 0.3
+	set_session(tf.Session(config=config))
+
+	model_path = sys.argv[2]
+	output_path = sys.argv[3]
 
 	siamese = load_model(model_path)
 
@@ -46,19 +60,24 @@ if __name__ == '__main__':
 	pairs = [np.zeros((sample*novel_class, img_size, img_size, 3)) for i in range(2)]
 
 	class_predictions = []
+	print('Predicting...\n')
 	for i in range(test_imgs.shape[0]):
 		for j in range(sample*novel_class):
 			pairs[0][j,:,:,:] = test_imgs[i].reshape(img_size, img_size, 3)
 			pairs[1][j,:,:,:] = novel_imgs[j].reshape(img_size, img_size, 3)
 
-		# compute distance
-		dist = siamese.predict(pairs)
+		# compute similarity
+		sim = siamese.predict(pairs).reshape((-1,))
 
-		pred = find_closest_class(dist, novel_label)
+		pred = find_closest_class(sim, novel_label)
 
 		class_predictions.append(pred)
+		progress(i+1, test_imgs.shape[0])
+	print('\n')
 
 	class_predictions = np.array(class_predictions)
+	print(list(class_predictions))
+	print(list(test_label))
 	acc = compute_acc(class_predictions, test_label)
 	print("Acc: ", acc)
 	'''
