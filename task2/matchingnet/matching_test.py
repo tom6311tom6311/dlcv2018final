41e50a01295e401d4dc6ce3a9f09501c48a06fd7
sonia1837 <r06942061@ntu.edu.tw>
2018-07-04 21:59:52 +0800

    modify test

--- task2/matchingnet/matching_test.py
+++ task2/matchingnet/matching_test.py
@@ -42,38 +42,11 @@ if __name__ == '__main__':
 	matching.load_weights(model_path, by_name = True)
 
 	novel_imgs, novel_label, test_imgs = io_data.read_test(data_path, test_path, sample = sample)
-	novel_imgs, novel_label, val_imgs, val_label = io_data.read_test2(data_path, os.path.join(data_path, 'novel'), sample = sample)
 
 	support_x_ = novel_imgs
 
 	tmp = [i for i in range(novel_class) for j in range(sample)]
 	support_y_ = np_utils.to_categorical(tmp, novel_class)
-
-	pred_val = []
-	for i in range(int(val_imgs.shape[0]/batch_size) + 1):
-		target_x = val_imgs[batch_size*i: min(val_imgs.shape[0], batch_size*(i+1))]
-		batch = target_x.shape[0]
-		support_x = np.array([support_x_ for i in range(batch)])
-		support_y = np.array([support_y_ for i in range(batch)])
-
-		if batch != batch_size:
-			support_x = np.vstack((support_x, support_x))
-			target_x = np.vstack((target_x, target_x))
-			support_y = np.vstack((support_y, support_y))
-			batch_ = batch_size
-			pred = matching.predict([support_x, target_x, support_y], batch_size = batch_)
-			pred = pred[0:batch]
-		else:
-			pred = matching.predict([support_x, target_x, support_y], batch_size = batch)
-		pred_val.append(pred)
-
-	pred_val = np.vstack(pred_val)
-	idx = np.argmax(pred_val, axis = 1)
-	novel_label = novel_label[np.arange(0, novel_label.shape[0], sample)]
-	pred_val = [novel_label[i] for i in idx]
-	acc = compute_acc(pred_val, val_label)
-	print("Acc: ", acc)
-
 
 	prediction = []
 	for i in range(int(test_imgs.shape[0]/batch_size) + 1):
@@ -93,6 +66,8 @@ if __name__ == '__main__':
 			pred = matching.predict([support_x, target_x, support_y], batch_size = batch)
 
 		prediction.append(pred)
+
+	novel_label = novel_label[np.arange(0, novel_label.shape[0], sample)]
 
 	prediction = np.vstack(prediction)
 	idx = np.argmax(prediction, axis = 1)
